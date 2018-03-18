69b851ecbc23f105c767cb1e246c904bbc8997e7
fmribeiro <fmribeiro21@gmail.com>
2018-03-18 18:45:23 -0300

    initial commit

---
+++ src/person.js
@@ -0,0 +1,11 @@
+export const isAdult = age => {
+  return age >= 18;
+};
+
+export const canDrink = age => {
+  return age >= 18;
+};
+
+export default age => {
+  return age >= 65;
+};
---
+++ src/utils.js
@@ -0,0 +1,7 @@
+console.log("utils.js is running");
+
+export const square = x => x * x;
+export const add = (a,b) => a + b;
+const substract = (a,b) => a - b;
+
+export{substract as default}
